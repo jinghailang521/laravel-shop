ade29eac48c12b38faa08088b9002d843369e8a5
景海浪 <768180562@qq.com>
2018-11-06 17:47:08 +0800

    邮箱验证中间件

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -5,17 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Laravel Shop') - Laravel 电商</title>
+    <title>@yield('title', 'Laravel Shop') - Laravel 电商教程</title>
     <!-- 样式 -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<boty>
-    <div id="app" class="{{ route_class() }}-page">
-        @include('layouts._header')
-        <div class="container">
-            @yield('content')
-        </div>
-        @include('layouts._footer')
+<body>
+<div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
+    <div class="container">
+        @yield('content')
     </div>
-</boty>
+    @include('layouts._footer')
+</div>
+<!-- JS 脚本 -->
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
 </html>
