1d5386fba7f7d025dd032d1e89168aae2f41defa
景海浪 <768180562@qq.com>
2018-11-12 15:46:39 +0800

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
     // 开始
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
     });
     // 结束
     Route::get('/email_verification/verfiy','EmailVerificationController@verify')->name('email_verification.verify');
     Route::get('/email_verification/send','EmailVerificationController@send')->name('email_verification.send');
-    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+
 });
